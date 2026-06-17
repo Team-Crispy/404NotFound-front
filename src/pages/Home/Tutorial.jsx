97b84d166b909f113307d6bdb1498b7286d20a47
parkjay0709 <soRjdla0709@gmail.com>
2026-06-17 10:50:02 +0900

    feat : 튜토리얼 화면 뒤로가기 버튼 추가

--- src/pages/Home/Tutorial.jsx
+++ src/pages/Home/Tutorial.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import Background from "../../assets/Back_tutorial.svg"
 import "../../styles/Tutorial.css"
 import OkBtn from '../../assets/Ok.svg'
+import BackBtn from '../../assets/back_Button.svg'
 
 function Tutorial() {
     const navigate = useNavigate();
@@ -14,6 +15,10 @@ function Tutorial() {
             className="tutorial-background"
             style={{ backgroundImage: `url(${Background})` }}
         >
+            <button type="button" className="tutorial-back-btn" onClick={handleNextPage}>
+                <img src={BackBtn} alt="뒤로가기" />
+            </button>
+
             <button type="button" className="ok_btn" onClick={handleNextPage}>
                 <img src={OkBtn} alt="확인" />
             </button>
